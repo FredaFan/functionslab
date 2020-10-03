a64fc8c17b98a0e4d9947beeda8d8011b280c9df
Jiayu Fan <Klein2339@gmail.com>
2020-10-03 20:23:46 +0100

    Initial structure

---
+++ webAPICall.js
@@ -0,0 +1,12 @@
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+    console.log(json[0])
+    json.forEach( (todo, index) => {
+      console.log(todo.title)
+
+    })
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
